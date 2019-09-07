ebf07455521b4f49324af7508748e7fa74d2bcb0
Tim <43763078+tulXoro@users.noreply.github.com>
2019-09-06 19:09:35 -0700

    Create JAK_MAIN.py

---
+++ src/JAK_MAIN.py
@@ -0,0 +1,8 @@
+# This is the main file for most operations of JAK. See JAK_Functions.py or the wiki
+# to understand the definitions or variables of certain things
+
+# Dependencies
+from JAK_Functions import *
+
+# This will be where all the programs are executed
+introduce()
